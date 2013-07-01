dee1415d2e4dd571510dfd6c7f4e96411eb4504d
Erlen Jose Barboza Mena <ebarboza10@gmail.com>
2013-07-01 07:33:22 -0600

    Files added

---
+++ UserProfileApi/Controllers/LoginController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace UserProfileApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        private Data.UserProfileContextDataContext userProfileContext = new Data.UserProfileContextDataContext();
+
+        [HttpPost]
+        public Models.UserResult Post(JObject data)
+        {
+            if (data != null)
+            {
+                dynamic json = data;
+                Models.User loginInfo = json.ToObject<Models.User>();
+                Models.User user = new Models.User();
+                List<Data.Token> tokenList = new List<Data.Token>();
+
+                Data.User userExists = userProfileContext.Users.Where(b => b.UserName == loginInfo.username && b.Password == loginInfo.password).FirstOrDefault();
+
+                
+
+                if (userExists != null)
+                {
+                    //clean tokens
+                    tokenList = userProfileContext.Tokens.Where(b => b.IdUsuario == userExists.Id).ToList();
+                    if (tokenList.Count > 0)
+                    {
+                        userProfileContext.Tokens.DeleteAllOnSubmit(tokenList);
+                        userProfileContext.SubmitChanges();
+                    }
+
+                    //register new token
+                    string strToken = Guid.NewGuid().ToString();
+                    Data.Token token = new Data.Token();
+
+                    token.Date = DateTime.Now;
+                    token.IdUsuario = userExists.Id;
+                    token.IdToken = strToken;
+
+                    userProfileContext.Tokens.InsertOnSubmit(token);
+
+                    userProfileContext.SubmitChanges();
+
+                    return new Models.UserResult()
+                    {
+                        result = true,
+                        token = strToken
+
+                    };
+
+                }
+                else
+                {
+                    return new Models.UserResult()
+                    {
+                        result = false,
+                        message = "Username or password does not exist."
+                    };
+
+                }
+            }
+            else
+            {
+                return new Models.UserResult()
+                {
+                    result = false,
+                    message = "Username or password are missing."
+                };
+            }
+        }
+             
+
+        [HttpGet]
+        public Data.User GetDetailsUser(string idToken)
+        {
+            Data.User userDetail = new Data.User();
+            Data.Token tokenDetail = new Data.Token();
+            if (idToken != null)
+            {
+
+                tokenDetail = userProfileContext.Tokens.Where(b => b.IdToken == idToken).FirstOrDefault();
+                if (tokenDetail != null)
+                {
+                    userDetail = userProfileContext.Users.Where(b => b.Id == tokenDetail.IdUsuario).FirstOrDefault();
+                }                
+            }
+            return userDetail;
+        }
+
+
+        [HttpDelete]
+        public Models.UserResult DeleteToken(string idToken)
+        {
+            Data.User userDetail = new Data.User();
+            Data.Token token = new Data.Token();
+            if (idToken != null)
+            {
+
+                token = userProfileContext.Tokens.Where(b => b.IdToken == idToken).FirstOrDefault();
+                if (token != null)
+                {
+
+                    userProfileContext.Tokens.DeleteOnSubmit(token);
+                    userProfileContext.SubmitChanges();
+
+                
+                return new Models.UserResult(){result = true };
+                }
+                else
+                    return new Models.UserResult() { result = false };
+
+            }
+            else
+            {
+                return new Models.UserResult() { result = false };
+            }
+        }
+
+    }
+}
---
+++ UserProfileApi/Models/Users.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace UserProfileApi.Models
+{
+
+
+    public class UsersDetail
+    {
+        public Int32 id { set; get; }
+        public string firstName { set; get; }
+        public string middleName { set; get; }
+        public string lastName { set; get; }
+        public string email { set; get; }
+        public string address { set; get; }
+        public string phone { set; get; }
+    }
+
+    public class User
+    {
+        [Required]        
+        public string username { set; get; }
+        [Required]
+        public string password { set; get; }
+    }
+
+    public class UserResult
+    {
+        public bool result { set; get; }
+        public string token { set; get; }
+        public string message { set; get; }
+    }
+}
